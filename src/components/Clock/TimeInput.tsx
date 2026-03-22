4ff4c0d0ecda8ab25bd1a53b0f1687c6dc1b9bc8
4ltynak <kineticjacks@gmail.com>
2026-03-22 19:23:16 +0800

    first commit

---
+++ src/components/Clock/TimeInput.tsx
@@ -0,0 +1,12 @@
+
+
+export default function TimeInput() {
+    return (
+        <div className="w-3/4 mx-auto">
+        <div className="flex justify-center">
+            <h3>Time Input</h3>
+        </div>
+        
+        </div>
+    )
+}
